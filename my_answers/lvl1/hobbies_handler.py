fd0560eafd6da95209e3cfa55daf5910daf62243
Eyal Leibovich <eyal.leibovich@autodesk.com>
2019-06-21 18:30:11 +0300

    started working on 9

--- my_answers/lvl1/hobbies_handler.py
+++ my_answers/lvl1/hobbies_handler.py
@@ -1,5 +1,21 @@
 from typing import NewType
 
-def hobbies():
+def hobbies(name, age: int, hobby, optional_hobbies = None, other = None):
+    if (not optional_hobbies):
+        optional_hobbies = ""
+    if (not other):
+        other = ""
+    while (type(age) != int):
+        try:
+            int(age)
+            break
+        except TypeError:
+            age = input('age must be a number, please enter a valid age\n')
+            
+
+    output = f'my name is {name}, my age is {age}.\nI like {hobby} {optional_hobbies}.'
+    return output
+
+if __name__ == "__main__":
     
-    return
+    print(hobbies('eyal', input('please enter you age\n'), 'playing', ['basketball', 'guitar']))

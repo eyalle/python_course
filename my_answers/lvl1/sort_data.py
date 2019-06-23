28b971a54506b96ef2b436811e5d6f1908eb6e6d
Eyal Leibovich <eyal.leibovich@autodesk.com>
2019-06-23 18:03:24 +0300

    updated the answer to match the question :D

--- my_answers/lvl1/sort_data.py
+++ my_answers/lvl1/sort_data.py
@@ -2,15 +2,32 @@ import sys
 sys.path.append("../../")
 from exercises.data import get_data
 
-def getName(dic):
-    return dic['name']
-
 data = get_data(20)
-sorted_list = []
-for item in data:
-    sorted_list.append((item['name'],item['address']))
 
-sorted_list = sorted(sorted_list)
-print(sorted_list)
+def getAddr(name):
+    addresses = []
+    for item in data:
+        if (name == item['name']):
+            addresses.append(item['address'])
+
+    return addresses
+
+
+
+names = []
+for item in data:
+    names.append(item['name'])
+
+list(set(names))
+
+output = []
+for item in names:
+    output.append((item, list(map(lambda item: item, getAddr(item)))))
+
+print(output)
+    
+
+# sorted_list = sorted(sorted_list)
+# print(sorted_list)
 # for item in sorted_list:
 #     print(f'{item[0]}, {item[1]}')

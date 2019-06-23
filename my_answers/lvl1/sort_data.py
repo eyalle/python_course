475acdd77afe8cc6f976226d3d407f6aa119d9ff
Eyal Leibovich <eyal.leibovich@autodesk.com>
2019-06-23 20:17:27 +0300

    Ex 12 working, Ex 13 working

--- my_answers/lvl1/sort_data.py
+++ my_answers/lvl1/sort_data.py
@@ -14,26 +14,33 @@ def getAddr(name):
 
     return addresses
 
-def add_random_postal(names_with_addresses):
+def add_random_postal(name_with_addresses):
     num = randint(0,9)*10000 + randint(0,9)*1000 + randint(0,9)*100 + randint(0,9)*10 + randint(0,9)
-    return (names_with_addresses, (f'{num}'))
+    if num < 9999:
+        num = (f'0{num}')
+    return (name_with_addresses + (f'{num}',))
 
+def sum_postal_codes(names_addresses_postals):
+    sum_of_postals = 0
+    for data_item in names_addresses_postals:
+        sum_of_postals += int(data_item[2])
+    return sum_of_postals
 
 names = set()
 for item in data:
     names.add(item['name'])
 
-
-
 output = []
 for name in names:
     output.append((name, list(map(lambda name: name, getAddr(name)))))
-    print(output[-1])
+    # print(output[-1])
 
 # print(output)
 
-for name_with_addresses in output:
-    name_with_addresses = add_random_postal(name_with_addresses)
-    print(name_with_addresses)
+for i in range(0, len(output)):
+    output[i] = add_random_postal(output[i])
+
+
+print(sum_postal_codes(output))
 
 

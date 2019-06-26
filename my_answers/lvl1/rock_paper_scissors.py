2717a37108c4bf1c79f0b19b175ba86499a02ca0
Eyal Leibovich <eyal.leibovich@autodesk.com>
2019-06-26 18:57:35 +0300

    Ex 16

--- my_answers/lvl1/rock_paper_scissors.py
+++ my_answers/lvl1/rock_paper_scissors.py
@@ -21,22 +21,20 @@ def get_comp_choice():
 
 def check_win(user, comp):
     if (RPS[comp]['WINS'] == user):
-        win_message = f'computer wins as {RPS[comp]["WINS"]} wins {user}\n'
+        win_message = f'computer wins as {comp} wins {user}\n'
     elif (RPS[user]['WINS'] == comp):
-        win_message = f'user wins as {RPS[user]["WINS"]} wins {comp}\n'
+        win_message = f'user wins as {user} wins {comp}\n'
     else:
         win_message = f'WEIRD: {user}\t{comp}\n'
     print(win_message)
-    # return winner
     pass
 
 user_choice = 'None'
 while (not user_choice in RPS):
-    # user_choice = input('please enter your choice: Rock, Paper or Scissors\n').upper()
     user_choice = (getpass.getpass('please enter your choice: Rock, Paper or Scissors\n')).upper()
 
 comp_choice = get_comp_choice()
-# print(f'user ==> comp\n{user_choice} ==> {comp_choice}\n')
+
 while (comp_choice == user_choice):
     print(f'TIE! {comp_choice} = {user_choice}')
     user_choice = getpass.getpass('let\'s try again: Rock, Paper or Scissors\n').upper()
